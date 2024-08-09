d4c76a83ba3eb028fab8bbbc86f16f9ea18ddacb
dolirajagukguk <dolipeaprayogirajagukguk@gmail.com>
2024-08-10 03:47:36 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -23,29 +23,3 @@ Route::get('/', function() {
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
-
-
-
-
-Route::get('/beritaWisata', function(){
-    return view('user.berita-wisata.index');
-});
-
-
-Route::get('/objekWisata', function(){
-    return view('user.objek-wisata.index');
-});
-
-Route::get('/detailobjekWisata', function(){
-    return view('user.objek-wisata.detail');
-});
-
-Route::prefix('admin')->group(function(){
-    Route::get('/destinasi', [App\Http\Controllers\AdminDestinasiController::class, 'AdminDestinationView'])->name('AdminDestinationView');
-    Route::get('/destinasiStore', [App\Http\Controllers\AdminDestinasiController::class, 'AdminDestinationStore'])->name('AdminDestinationStore');
-    Route::post('/createDestinasi', [App\Http\Controllers\AdminDestinasiController::class, 'StoreDestination'])->name('destinations.store');
-    Route::delete('/destinasi/{destinasi}', [App\Http\Controllers\AdminDestinasiController::class, 'softDelete'])->name('destinasi.softDelete');
-    Route::get('/destinasi/{id}/edit', [App\Http\Controllers\AdminDestinasiController::class, 'edit'])->name('destinasi.edit');
-    Route::put('/destinasi/{id}', [App\Http\Controllers\AdminDestinasiController::class, 'update'])->name('destinasi.update');
-    });
-
